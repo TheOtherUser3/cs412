970129c5c10edb2c245638299bc845da908c9d1a
TheOtherUser3 <dawsonmaska@gmail.com>
2025-12-09 02:32:39 -0500

    more testing

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+sys.path.insert(0, str(Path.home() / ".local/lib/python3.12/site-packages"))
+sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs412.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
